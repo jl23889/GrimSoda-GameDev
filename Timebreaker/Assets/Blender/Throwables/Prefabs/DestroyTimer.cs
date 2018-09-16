f887ef690b0aa7eee19378188351594d96b5e24c
Joon Lee <jlee23889@gmail.com>
2018-09-15 23:32:05 -0500

    added throwables folder to blender assets; start work on destructible barrel

---
+++ Timebreaker/Assets/Blender/Throwables/Prefabs/DestroyTimer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTimer : MonoBehaviour {
+
+    public float timeToDestroy = 3;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Destroy());
+    }
+
+    IEnumerator Destroy()
+    {
+        yield return new WaitForSecondsRealtime(timeToDestroy);
+        Destroy(gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
