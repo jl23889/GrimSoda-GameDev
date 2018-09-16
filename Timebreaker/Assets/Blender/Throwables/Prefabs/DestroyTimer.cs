fe8529d19ee756903b64dc8bcc06df3b1624fa96
Joon Lee <jlee23889@gmail.com>
2018-09-16 00:03:40 -0500

    added explosionforce to the barrel fragments rigidbodies

--- Timebreaker/Assets/Blender/Throwables/Prefabs/DestroyTimer.cs
+++ Timebreaker/Assets/Blender/Throwables/Prefabs/DestroyTimer.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class DestroyTimer : MonoBehaviour {
 
-    public float timeToDestroy = 3;
+    public float timeToDestroy = 3f;
+    public float explosionRadius = 5f;
+    public float power = 1000f;
+    public float upForce = 3f;
 
     // Use this for initialization
     void Start () {
         StartCoroutine(Destroy());
+        // add explosion force to the children
+        Vector3 explosionPos = transform.position;
+        Rigidbody[] rbFragments = gameObject.GetComponentsInChildren<Rigidbody>();
+
+        foreach (Rigidbody rb in rbFragments)
+        {
+            Debug.Log("exploding!! " + rb);
+            Debug.Log("explosionPos" + explosionPos);
+            rb.AddExplosionForce(power, explosionPos, explosionRadius, upForce);
+        }
     }
 
     IEnumerator Destroy()
