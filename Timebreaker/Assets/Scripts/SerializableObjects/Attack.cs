d5b5849e9a2f74fe23ca50f9b75217a963544a3e
Joon Lee <jlee23889@gmail.com>
2018-09-28 06:25:55 -0500

    create seralizableObject definitions for characterList, character, and attacks

---
+++ Timebreaker/Assets/Scripts/SerializableObjects/Attack.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Attack", menuName = "Define New Attack")]
+public class Attack : ScriptableObject
+{
+    public string attackName = "New Attack";
+    public AnimationClip animationClip;
+    public int damage = 1;
+    public string attackLimb;
+    public Vector3 hitboxSize = new Vector3(1,1,1);
+    public bool knockback = false;
+    public bool knockdown = false;
+    public bool knockup = false;
+}
---
+++ Timebreaker/Assets/Scripts/SerializableObjects/Character.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Character", menuName = "Define New Character")]
+public class Character : ScriptableObject
+{
+    public string charName = "New Character";
+    public GameObject charPrefab;
+    public int healthTotal = 100;
+    public int staminaTotal = 100;
+    public float runSpeed = 1;
+    public List<Attack> attackList;
+}
