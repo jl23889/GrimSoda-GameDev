edbdab3cc5c9779e64f5fe2909fbb2bac47b8f86
Norman Aberin <npaberin@gmail.com>
2018-10-22 01:11:35 -0500

    More stuff scene managing

---
+++ Timebreaker/Assets/PlayScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayScript : MonoBehaviour {
+
+    private bool started = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void setStarted()
+    {
+        started = !started;
+    }
+
+    public void playGame()
+    {
+        // Show animation on sides
+        // Make title smaller and higher
+        // show real menu
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKey && !started)
+        {
+            // when back button is pressed, return to title page and set started = false;
+            setStarted();
+            playGame();
+        }
+	}
+}
