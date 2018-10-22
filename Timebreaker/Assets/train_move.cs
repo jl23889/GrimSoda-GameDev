9fcbb80157ceccedee84fd249f841287eb79a165
beleafs <38118864+beleafs@users.noreply.github.com>
2018-10-21 21:21:49 -0500

    train behavior

--- Timebreaker/Assets/train_move.cs
+++ Timebreaker/Assets/train_move.cs
@@ -3,44 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class train_move : MonoBehaviour {
-    private Vector3 _moving_direction;
+    private Vector3 _moving_in_direction;
+    private float _direction = 1;
     private float _currentSpeed;
-    public float _speed = 0.1f;
-    public float stoptime = 20f;
+    public float _speed = 1f;
+
+    private float _inStationStopTime;
+    public float _inStationTimeMin = 2f;
+    public float _inStationTimeMax = 10f;
+
+    private float _turnAroundStopTime;
+    public float _turnAroundTimeMin = 2f;
+    public float _turnAroundTimeMax = 10f;
+
     private float time_counter = 0;
+    private float time_counter2 = 0;
+    private bool _isAccelerating = false;
+
 
 	// Use this for initialization
 	void Start () {
         _currentSpeed = _speed;
-        _moving_direction = new Vector3(0f, 0f, _currentSpeed);
+        _moving_in_direction = new Vector3(0f, 0f, _currentSpeed * _direction);
+        _turnAroundStopTime = Random.Range(_turnAroundTimeMin, _turnAroundTimeMax);
+        _inStationStopTime = Random.Range(_inStationTimeMin, _inStationTimeMax);
     }
 
     void FixedUpdate()
     {
-        if (transform.localPosition.z < -1f)
+        if (transform.localPosition.z < -2f && _direction == -1)
         {
-            _moving_direction = new Vector3(0f, 0f, _speed);
+            TurnAround();
+            _direction = 1;            
         }
-        if (transform.localPosition.z > 1f)
+        
+        if (transform.localPosition.z > 2f && _direction == 1)
         {
-            _moving_direction = new Vector3(0f, 0f, -_speed);
+            TurnAround();
+            _direction = -1;           
         }
 
-        if (transform.localPosition.z > -0.5f && transform.localPosition.z < -0.1f)
+        StopAtTurning(_turnAroundStopTime);
+
+        if (!_isAccelerating)
+            Stoping();
+        else
+            Accelerating();
+
+        _moving_in_direction = new Vector3(0f, 0f, _currentSpeed * _direction);
+        transform.position = transform.position + _moving_in_direction;
+    }
+
+    private void Accelerating()
+    {
+        if (Mathf.Abs(transform.localPosition.z) < 0.05f)
+        {
+            _currentSpeed = _speed / 10f;
+        }
+        else if (Mathf.Abs(transform.localPosition.z) < 0.5f && Mathf.Abs(transform.localPosition.z) > 0.05f)
         {
             _currentSpeed = Mathf.Lerp(0, _speed, Mathf.Abs(transform.localPosition.z) / 0.5f);
-            _moving_direction = new Vector3(0f, 0f, _currentSpeed);
         }
-        else if (transform.localPosition.z > -0.1f && transform.localPosition.z < -0.05f)
+        else
         {
-            _moving_direction = new Vector3(0f, 0f, _speed / 5f);
+            _currentSpeed = _speed;
+            _isAccelerating = false;
         }
-        else if (transform.localPosition.z > 0f)
+    }
+
+    private void Stoping()
+    {
+        if (Mathf.Abs(transform.localPosition.z) < 0.5f && Mathf.Abs(transform.localPosition.z) > 0.05f)
         {
-            _moving_direction = Vector3.zero;
+            _currentSpeed = Mathf.Lerp(0, _speed, Mathf.Abs(transform.localPosition.z) / 0.5f);
         }
+        else if (Mathf.Abs(transform.localPosition.z) < 0.05f)
+        {
+            StopAtStation(_inStationStopTime);
+        }
+    }
 
+    private void StopAtStation(float stoptime)
+    {
+        _currentSpeed = 0f;
+        time_counter = time_counter + Time.fixedDeltaTime;
+        if (time_counter > stoptime)
+        {
+            _isAccelerating = true;
+            time_counter = 0;
+        }
+    }
 
-        transform.position = transform.position + _moving_direction;
+    private void StopAtTurning(float stoptime)
+    {
+        _currentSpeed = 0f;
+        time_counter2 = time_counter2 + Time.fixedDeltaTime;
+        if (time_counter2 > stoptime)
+        {
+            _currentSpeed = _speed;
+            
+        }
+    }
+
+    private void TurnAround()
+    {
+        transform.rotation = Quaternion.Euler(0f, (90f + 90f *_direction), 0f);
+        time_counter2 = 0;
     }
 }
