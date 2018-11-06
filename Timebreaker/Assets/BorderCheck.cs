a6cf28c869dd5f2ec3b2d3ed8255a7b388705264
Yunxi Liu <38118864+beleafs@users.noreply.github.com>
2018-11-06 11:13:37 -0600

    PlayerRespawn

---
+++ Timebreaker/Assets/BorderCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Border")
+        {
+            transform.position = new Vector3(33.6f, 5.3f,72.3f);
+            Debug.Log("1");
+        }
+    }
+}
