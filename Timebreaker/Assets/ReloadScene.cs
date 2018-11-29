1a24040ba34820ec294b3bd8b0bd69fe7e51f085
Yunxi Liu <38118864+beleafs@users.noreply.github.com>
2018-11-28 20:13:08 -0600

    reload

---
+++ Timebreaker/Assets/ReloadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadScene : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }		
+	}
+}
